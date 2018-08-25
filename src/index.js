d99fed336b749381d881757f8a822ca6c49921c2
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2018-08-25 09:50:32 -0700

    updates

--- src/index.js
+++ src/index.js
@@ -18,13 +18,16 @@ import { CookiesProvider } from 'react-cookie';
 // ReactDOM.render(render(App), document.getElementById('root'));
 // registerServiceWorker();
 
-const Link = ({children, href}) => {
+const Link = ({children, href, locationSetter}) => {
   const style = {
     cusrsor: 'pointer'
   };
 
   const changeHash = () => {
     window.location.href = href;
+
+    var location = window.location.hash.replace(/^#\/?|\/$/g, '').split('/');
+    locationSetter(location);
   }
 
   return (
@@ -35,41 +38,54 @@ const Link = ({children, href}) => {
 }
 
 class Application extends React.Component {
-  render() {
-    return <Link href='#n'>
-    <div><h1>Index Page</h1></div>
-  </Link>
+  constructor(props){
+    super();
+    this.state = {
+      location: props.location
+    }
 
-    switch (this.props.location[0]) {
+  }
+  setLocation = location => {
+    this.setState({location});
+  }
+
+  componentDidMount() {
+    const location = '';
+    setTimeout(() => {
+      setLocation('');
+    }, 2000)
+  }
+
+  render() {
+
+    switch (this.state.location[0]) {
       case '':
-        return <Link href='#n'>
+        return <Link locationSetter={this.setLocation} href='#n'>
           <div><h1>Index Page</h1></div>
         </Link>
       case 'n':
-        return <Link href='#1'>
+        return <Link locationSetter={this.setLocation} href='#1'>
           <div><h1>n-word Page</h1></div>
         </Link>
       case '1':
-        return <Link href='#2'>
+        return <Link locationSetter={this.setLocation} href='#2'>
           <div><h1>1 Page</h1></div>
         </Link>
-      case '2':
-        return <Link href='#3'>
+      case '2':        
+        return <Link locationSetter={this.setLocation} href='#3'>
           <div><h1>2 Page</h1></div>
         </Link>
       default:
-        return <div><h1>Not Found</h1></div>;
+        return <Link locationSetter={this.setLocation} href=''>
+          <div><h1>Not Found</h1></div>;
+        </Link>
     }
   }
 };
 
-// Split location into `/` separated parts, then render `Application` with it
-function handleNewHash() {
-  var location = window.location.hash.replace(/^#\/?|\/$/g, '').split('/');
-  var application = <Application location={location} />;
-  ReactDOM.render(application, document.getElementById('root'));
-}
-
-// Handle the initial route and browser navigation events
-window.addEventListener('hashchange', handleNewHash, false);
-handleNewHash()
+//handleNewHash()
+window.addEventListener('loaded')
+ReactDOM.render(<Application location={['']}/>, document.getElementById('root'));
+// setTimeout(() => {
+//   document.querySelector('h1').click();
+// }, 2000);
